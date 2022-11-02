21e0b03bcdcd4e0f800a371b497d4be27a8d892b
Ethan-Prieur1 <Ethan-Prieur1@users.noreply.github.com>
2022-11-02 13:55:06 +0000

    Prettified Code!

--- src/Truck.ts
+++ src/Truck.ts
@@ -1,42 +1,46 @@
 /*
-* This program is a truck class.
-*
-* @author  Ethan Prieur
-* @version 1.0
-* @since   2020-11-01
-*/
+ * This program is a truck class.
+ *
+ * @author  Ethan Prieur
+ * @version 1.0
+ * @since   2020-11-01
+ */
 
 class Truck extends Vehicle {
-        private licensePlate: string
-        
-        constructor(colour: string, licensePlate: string, doorAmount: number, maxSpeed: number) { 
+  private licensePlate: string
 
-          this.licensePlate = licensePlate
-          this.maxSpeed = maxSpeed
-        }
+  constructor(
+    colour: string,
+    licensePlate: string,
+    doorAmount: number,
+    maxSpeed: number
+  ) {
+    this.licensePlate = licensePlate
+    this.maxSpeed = maxSpeed
+  }
 
-        public status (): void {
-        console.log(
+  public status(): void {
+    console.log(
       `        --> Speed: ${super.getSpeed()}
         --> MaxSpeed: ${super.getMaxSpeed()}
         --> License Plate: ${this.licensePlate}
         --> Color: ${super.getColor()}`
     )
-        } 
+  }
 
-        public getLicensePlate (): string {
-        return this.licensePlate
-        }
+  public getLicensePlate(): string {
+    return this.licensePlate
+  }
 
-        public setLicensePlate (newLicensePlate: string): void {
-        this.licensePlate = newLicensePlate
-        }
+  public setLicensePlate(newLicensePlate: string): void {
+    this.licensePlate = newLicensePlate
+  }
 
-        public provideAir (airPressure: number): void {
-        super.setSpeed(super.getSpeed() - airPressure / 2)
+  public provideAir(airPressure: number): void {
+    super.setSpeed(super.getSpeed() - airPressure / 2)
 
-        if (super.getSpeed() < 0) {
-        super.setSpeed(0)
+    if (super.getSpeed() < 0) {
+      super.setSpeed(0)
     }
   }
 }
